2e17c03821180d74397a489726a46ed4cd8e1d56
vrajeshgokani <vrajeshgokani41@gmail.com>
2023-03-12 02:33:11 +0530

    App
    App

---
+++ picbucks/lib/pages/gallery_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class GalleryPage extends StatefulWidget {
+  const GalleryPage({super.key});
+
+  @override
+  State<GalleryPage> createState() => _GalleryPageState();
+}
+
+class _GalleryPageState extends State<GalleryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
